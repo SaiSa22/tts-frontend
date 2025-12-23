c06a65e4c2fcc5a5571a1a0a8630a6ee902308b7
SaiSa22 <ssabti@gmail.com>
2025-12-22 21:45:12 -0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // TODO: Replace with your actual values from Supabase Settings -> API
-const supabaseUrl = 'YOUR_SUPABASE_PROJECT_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://qwifaauejfmhuhyyjius.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aWZhYXVlamZtaHVoeXlqaXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMDE1MTYsImV4cCI6MjA4MTY3NzUxNn0.sm0OYygWgRPUvUuyb3R1lC8KXD0C9l1tJ5PduUHHzOU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
